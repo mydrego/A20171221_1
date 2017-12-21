a34d7cccea602ccbfffaff217beb283f4fd9d7d9
MyDreGo <james@mydrego.com>
2017-12-21 21:50:50 +0800

    Initial commit map

---
+++ app/src/main/java/com/example/student/a20171221_1/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.student.a20171221_1;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityCompatApi23;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    Mylocation mylocation;
+    public TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mylocation = new Mylocation();
+        textView = (TextView) findViewById(R.id.textView);
+        // 詢問是否開啟 定位 權限
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 9001
+        );
+        //
+    }
+
+    private void setMylocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mylocation);
+    }
+
+
+    public void click1(View view) {
+        setMylocation();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 9001) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setMylocation();
+            }
+        }
+    }
+
+    public void click2(View view) {
+        locationManager.removeUpdates(mylocation);
+    }
+
+    class Mylocation implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("A11", location.toString());
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            textView.setText("GPS = \n" + lng + "\n" + lat);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+}
+
+
